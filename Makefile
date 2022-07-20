63815ee05c2b68c4452b5d88d8c4738d07eaa7d2
edwmrk <markeviciuseidvilas@gmail.com>
2022-07-20 23:24:09 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+.RECIPEPREFIX = >
+
+out/%: src/%.scm
+> @ mkdir -p $$(dirname $@)
+> @ cp $< $@
+> @ chmod +x $@
+
+build: $(shell find src -type f | sed 's|^src/|out/|' | sed 's|.scm$/||')
+
+clean:
+> @ rm -rf out
+
+install: build
+> @ mkdir -p $(DESTDIR)$(PREFIX)/bin
+> @ cp -r out/. $(DESTDIR)$(PREFIX)/bin
+
+uninstall:
+> @ rm -f $(DESTDIR)$(PREFIX)/bin/sxml2xml
+> @ rm -f $(DESTDIR)$(PREFIX)/bin/xml2sxml
+
+reinstall: uninstall install
---
+++ src/sxml2xml.scm
@@ -0,0 +1,48 @@
+#!/bin/guile --no-auto-compile
+!#
+
+(use-modules
+  (sxml simple))
+
+(define (get-0th-argument)
+  (list-ref (command-line) 0))
+(define (get-1st-argument)
+  (list-ref (command-line) 1))
+(define (get-2nd-argument)
+  (list-ref (command-line) 2))
+
+(define %program-name
+  (basename (get-0th-argument)))
+
+(when (or (= (length (command-line)) 1)
+          (equal? (get-1st-argument) "--help")
+          (equal? (get-1st-argument) "-h"))
+  (display
+    (string-append
+      "Usage: " %program-name " [options] [file]\n"
+      "\n"
+      "options: \n"
+      "  -h, --help  show this help message and exit\n"))
+  (exit))
+
+(when (and (string-prefix? "-" (get-1st-argument))
+           (not (equal? (get-1st-argument) "--")))
+  (display
+    (string-append
+      "Unrecognized option: '" (get-1st-argument) "'\n"
+      "Try '" %program-name " --help' for more information.\n")
+    (current-error-port))
+  (exit))
+
+(let ((input-file (if (equal? (get-1st-argument) "--")
+                    (get-2nd-argument)
+                    (get-1st-argument))))
+  (if (access? input-file R_OK)
+    (begin
+      (sxml->xml
+        (call-with-input-file input-file read))
+      (display "\n"))
+    (display
+      (string-append
+        "Can't get access to the specified file: '" input-file "'\n")
+      (current-error-port))))
